a1cd9ed285941c2997433dde3bb7dd1be15d0210
Paula Vasconcelos Gueiros <pvgueiros@gmail.com>
2024-10-07 22:38:37 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Paula Vasconcelos Gueiros on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
