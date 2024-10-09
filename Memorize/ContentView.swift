e67d9d9a7a525a2d06be2178801bdd7c4d9d6839
Paula Vasconcelos Gueiros <pvgueiros@gmail.com>
2024-10-09 00:41:53 -0300

    CardView and isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
+///
+/// ": View" means "behaves like a view" - not it's superclass! Means it conforms to the protocol View
+/// Functional programming is about behavior
+///
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.blue)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(lineWidth: 5)
+                Text("👑")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 20)
+            }
+        }
     }
 }
 
